9dbb9f23b111d9e740074cf3a770297c3cb57bf9
Arth Doshi <arth.doshi.33@gmail.com>
2019-11-17 15:02:30 +0000

    Welcome revenge OS

--- revengeos_santoni.mk
+++ revengeos_santoni.mk
@@ -16,26 +16,21 @@
 
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
-# Inherit some common Havoc-OS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common Revenge OS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4
-PRODUCT_NAME := havoc_santoni
+PRODUCT_NAME := revengeos_santoni
 BOARD_VENDOR := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Official
-export export HAVOC_BUILD_TYPE=Official
-
-# Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.havoc.maintainer=STARGAZER
+#export export HAVOC_BUILD_TYPE=Official
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
+TARGET_BOOT_ANIMATION_RES := 720
 	
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
