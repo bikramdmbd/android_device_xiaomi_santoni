3bff89a66e6bcb85254bad3b4c589fdc297ee5d9
Nick C <ctwoon@suicide.today>
2020-05-30 12:50:04 +0300

    santoni: update bdroid_buildcfg values from caf
    https: //source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/bluetooth-commonsys-intf/tree/build/qva/config/bdroid_buildcfg.h?h=LA.UM.8.6.2.r1-08400-89xx.0
    Change-Id: I5a95bec3db32eb4f6f83f33087612cf43294fc5b
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,8 +20,9 @@
 
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    16
+#define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
+#define GATT_MAX_PHY_CHANNEL  10
 
 // skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED TRUE
@@ -30,8 +31,7 @@
 #define BTM_WBS_INCLUDED TRUE
 #define BTIF_HF_WBS_PREFERRED TRUE
 
-/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
-#define AVDT_NUM_SEPS 12
+#define AVDT_NUM_SEPS 35
 
 /* Enable HFP WBS feature */
 #define BTIF_HF_CLIENT_WBS_INCLUDED TRUE
