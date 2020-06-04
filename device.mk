d94997e081e0f9f540a13a028954126e2bd553d6
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2020-06-04 08:23:46 +0700

    santoni: Add privapp permission for Snap
    - from
    https://github.com/LineageOS/android_vendor_lineage/blob/lineage-17.1/config/permissions/privapp-permissions-lineage-system.xml
    
    Change-Id: I36642b1c4d95a783850d6e92f884318402bbc177

--- device.mk
+++ device.mk
@@ -375,6 +375,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
+# Privapp permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-lineage.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lineage.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
