50c4c899f343bfb667259509667e6f8aa63bd4cf
bikram557 <kitkatmukherjee2015@gmail.com>
2020-09-11 04:44:36 +0000

    [TMP]santoni: build tmp sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -231,6 +231,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # SELinux
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-tmp
 #include device/qcom/sepolicy-legacy-um/sepolicy.mk
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
