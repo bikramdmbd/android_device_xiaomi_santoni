48132e7c71fd72984a9f5faaa8279255fa483ad3
bikram557 <kitkatmukherjee2015@gmail.com>
2020-10-03 07:41:41 +0000

    santoni: nuke telephony-ext

--- device.mk
+++ device.mk
@@ -425,10 +425,7 @@ PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
-    qti_telephony_utils.xml \
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+    qti_telephony_utils.xml
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
