5e6f50ccae0661716c003a74bbc9fb5df5fd3d83
Akash98Sky <a98mondal@gmail.com>
2020-11-10 18:34:01 +0400

    santoni: Build power stats HAL

--- device.mk
+++ device.mk
@@ -354,7 +354,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Platform
 PRODUCT_BOARD_PLATFORM := msm8996
